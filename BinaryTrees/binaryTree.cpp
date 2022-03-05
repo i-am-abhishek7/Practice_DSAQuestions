db2cc11051c7d8133298e1ec37d9731f57724dac
Abhishek <akthakuron120@gmail.com>
2022-03-05 16:29:12 +0530

    maxDepth of binary tree or balanced binary tree etc

--- BinaryTrees/binaryTree.cpp
+++ BinaryTrees/binaryTree.cpp
@@ -44,13 +44,52 @@ vector<vector<int>> levelOrderTraversal(BinaryTreeNode* root) {
   return ans;
 }
 
+vector<int> preOrderIterative(BinaryTreeNode* root) {
+  vector<int> ans;
+  if(root == NULL) return ans;
+  stack<BinaryTreeNode*> st;
+  st.push(root);
+
+  while(!st.empty()) {
+    root = st.top();
+    st.pop();
+    ans.push_back(root->data);
+    if(root->right != NULL) st.push(root->right);
+    if(root->left != NULL) st.push(root->left);
+  }
+  return ans;
+}
+
+int maxDepth(BinaryTreeNode* root) {
+  if(root == NULL) return 0;
+  return 1 + max(maxDepth(root->left), maxDepth(root->right));
+}
+
+int dfsHeight(BinaryTreeNode* root) {
+  if(root == NULL) return 0;
+  int leftHeight = dfsHeight(root->left);
+  if(leftHeight == -1) return -1;
+  int rightHeight = dfsHeight(root->right);
+  if(rightHeight == -1) return -1;
+  if(abs(rightHeight - leftHeight) > 1) return -1;
+  return 1 + max(leftHeight, rightHeight);
+}
+
+bool balancBinaryTree(BinaryTreeNode* root) {
+  return dfsHeight(root) != -1;
+}
+
 int main() {
   BinaryTreeNode* root = new BinaryTreeNode(10);
   root->left = new BinaryTreeNode(20);
-  root->left->left = new BinaryTreeNode(40);
-  root->left->right = new BinaryTreeNode(50);
   root->right = new BinaryTreeNode(30);
-  root->right->left = new BinaryTreeNode(60);
+  root->right->left = new BinaryTreeNode(40);
+  root->right->left->left = new BinaryTreeNode(50);
+  root->right->right = new BinaryTreeNode(60);
+  cout << "Height Of Binary Tree is: " << maxDepth(root) << endl;
+  cout << balancBinaryTree(root);
+  // vector<int> result = preOrderIterative(root);
+  // for(int i = 0; i < result.size(); i++) cout << result[i] << " ";
   // inOrderTraversal(root);
   // preOrderTraversal(root);
   // postOrderTraversal(root);
