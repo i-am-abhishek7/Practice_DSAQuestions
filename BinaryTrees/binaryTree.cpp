2706836fcd85fc6937e5e6a756962e24c7ef0940
Abhishek <akthakuron120@gmail.com>
2022-03-06 18:36:05 +0530

    Diameter of binary tree with optimization time complexity reduced to O(n).

--- BinaryTrees/binaryTree.cpp
+++ BinaryTrees/binaryTree.cpp
@@ -89,6 +89,31 @@ int diameter(BinaryTreeNode* root) {
   return max(option1, max(option2, option3));
 }
 
+// Time complexity of this code is O(n).
+pair<int, int> heightDiameter(BinaryTreeNode* root) {
+  if(root == NULL) {
+    pair<int, int> p;
+    p.first = 0;
+    p.second = 0;
+    return p;
+  }
+
+  pair<int, int> leftAns = heightDiameter(root->left);
+  pair<int, int> rightAns = heightDiameter(root->right);
+
+  int leftHeight = leftAns.first;
+  int rightHeight = rightAns.first;
+  int leftDiameter = leftAns.second;
+  int rightDiameter = rightAns.second;
+
+  int height = 1 + max(leftHeight, rightHeight);
+  int diameter = max(leftHeight + rightHeight, max(leftDiameter, rightDiameter));
+
+  pair<int, int> p;
+  p.first = height;
+  p.second = diameter;
+  return p;
+}
 
 int main() {
   BinaryTreeNode* root = new BinaryTreeNode(1);
@@ -102,6 +127,8 @@ int main() {
   cout << balancBinaryTree(root);
   cout << endl;
   cout << "The diameter of binary tree is: " << diameter(root) << endl;
+  pair<int, int> p = heightDiameter(root);
+  cout << "Height: " << p.first << endl <<  "Diameter: " << p.second << endl;
   // vector<int> result = preOrderIterative(root);
   // for(int i = 0; i < result.size(); i++) cout << result[i] << " ";
   // inOrderTraversal(root);
