bc4df20d757b526ed95e0d3f0df3ca2aea838fdb
Abhishek <akthakuron120@gmail.com>
2022-04-07 14:34:07 +0530

    Frog jump k steps using meomization.

--- DP/frogJumpKSteps.cpp
+++ DP/frogJumpKSteps.cpp
@@ -16,11 +16,29 @@ int f1(int idx, int heights[], int k) {
   return mmSteps;
 }
 
+
+// Using Meomization
+// Time Complxity: O(n) * O(k)
+// Space Complexity: O(n)
+int f2(int idx, int heights[], int k, vector<int>& dp) {
+  if(idx == 0) return 0;
+
+  if(dp[idx] != -1) return dp[idx];
+  int mmSteps = INT_MAX;
+  for(int i = 1; i <= k; i++) {
+    int jumps = f2(idx-i, heights, k, dp) + abs(heights[idx] - heights[idx-i]);
+    mmSteps = min(jumps, mmSteps);
+  }
+  return dp[idx] = mmSteps;
+}
+
 int main() {
   int n;
   cin >> n;
   int arr[n];
   for(int i = 0; i < n; i++) cin >> arr[i];
-  cout << "Energy used by the frog is: " << f1(n-1, arr, 5) << endl;
+  // cout << "Energy used by the frog is: " << f1(n-1, arr, 5) << endl;
+  vector<int> dp(n+1, -1);
+    cout << "Energy used by the frog is: " << f2(n-1, arr, 5, dp) << endl;
   return 0;
 }
