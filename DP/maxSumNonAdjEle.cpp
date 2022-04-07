29a3aa301bfb73ebf9aa664d7df2cfab3635c977
Abhishek <akthakuron120@gmail.com>
2022-04-07 17:46:51 +0530

    Max sum of non adjacent elements using recursion and memeoization.

---
+++ DP/maxSumNonAdjEle.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Using Recursion
+// Time Complexity: O(2^n)
+// Space Compexity: O(n)
+int f(vector<int> nums, int index) {
+  if(index == 0) return nums[index];
+  if(index < 0) return 0;
+
+  int pick = nums[index] + f(nums, index-2);
+  int notPick = f(nums, index-1);
+
+  return max(pick, notPick);
+}
+
+// Using Memoization
+// Time Complexity: O(n)
+// Space Complexity: O(n) + O(n)
+int f1(vector<int> nums, int index, vector<int>& dp) {
+  if(index == 0) return nums[index];
+  if(index < 0) return 0;
+
+  if(dp[index] != -1) return dp[index];
+
+  int pick = nums[index] + f1(nums, index-2, dp);
+  int notPick = f1(nums, index-1, dp);
+  
+  return dp[index] = max(pick, notPick);
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> nums(n);
+  for(int i = 0; i < nums.size(); i++) cin >> nums[i];
+  // cout << "The maximum sum of non adjacent elements is: " << f(nums, nums.size()-1);
+  vector<int> dp(n, -1);
+  cout << "The maximum sum of non adjacent elements is: " << f1(nums, nums.size()-1, dp);
+  return 0;
+}
