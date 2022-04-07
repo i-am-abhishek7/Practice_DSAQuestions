cd759e3133c08ec8a05873003a271663de107989
Abhishek <akthakuron120@gmail.com>
2022-04-07 18:03:46 +0530

    Using Tabulation.

--- DP/maxSumNonAdjEle.cpp
+++ DP/maxSumNonAdjEle.cpp
@@ -37,6 +37,18 @@ int main() {
   for(int i = 0; i < nums.size(); i++) cin >> nums[i];
   // cout << "The maximum sum of non adjacent elements is: " << f(nums, nums.size()-1);
   vector<int> dp(n, -1);
-  cout << "The maximum sum of non adjacent elements is: " << f1(nums, nums.size()-1, dp);
+  // cout << "The maximum sum of non adjacent elements is: " << f1(nums, nums.size()-1, dp);
+
+  // Using Tabulation Method
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
+  dp[0] = nums[0];
+  for(int i = 1; i < n; i++) {
+    int pick = nums[i];
+    if(i > 1) pick += dp[i-2];
+    int notPick = 0 + dp[i-1];
+    dp[i] = max(pick, notPick);
+  }
+  cout << "The maximum sum of non adjacent elements is: " << dp[n-1];
   return 0;
 }
