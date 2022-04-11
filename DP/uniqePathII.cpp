678dc05d654929ba10b0e429cabd5aee568bbe3a
Abhishek <akthakuron120@gmail.com>
2022-04-11 17:00:50 +0530

    Unique Path using Tabulation.

--- DP/uniqePathII.cpp
+++ DP/uniqePathII.cpp
@@ -35,7 +35,25 @@ int main() {
     }
   }
   // cout << "Ans: " << f(n-1, m-1, nums);
-  vector<vector<int>> dp(n, vector<int>(m, -1));
-  cout << "Ans: " << f1(n-1, m-1, nums, dp);
+  // vector<vector<int>> dp(n, vector<int>(m, -1));
+  // cout << "Ans: " << f1(n-1, m-1, nums, dp);
+
+  // Using Tabulation
+  int dp[n][m];
+
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < m; j++) {
+      if(nums[i][j] == -1) dp[i][j] = 0;
+      else if(i == 0 && j == 0) dp[i][j] = 1;
+      else {
+        int up = 0;
+        int left = 0;
+        if(i > 0) up = dp[i-1][j];
+        if(j > 0) left = dp[i][j-1];
+        dp[i][j] = up + left;
+      }
+    }
+  }
+  cout << "Ans: " << dp[n-1][m-1] << endl;
   return 0;
 }
