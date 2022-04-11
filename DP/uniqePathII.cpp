e16987a53e0e0883533dc66dc71739638de16588
Abhishek <akthakuron120@gmail.com>
2022-04-11 16:57:40 +0530

    Unique Path using Memoization.

--- DP/uniqePathII.cpp
+++ DP/uniqePathII.cpp
@@ -12,6 +12,19 @@ int f(int i, int j, vector<vector<int>>& mat) {
   return up + left;
 }
 
+// Using Memoization
+int f1(int i, int j, vector<vector<int>>& mat, vector<vector<int>>& dp) {
+  if(i >= 0 && j >= 0 && mat[i][j] == -1) return 0;
+  if(i == 0 && j == 0) return 1;
+  if(i < 0 || j < 0) return 0;
+
+  if(dp[i][j] != -1) return dp[i][j];
+
+  int up = f1(i-1, j, mat, dp);
+  int left = f1(i, j-1, mat, dp);
+  return dp[i][j] = up + left;
+}
+
 int main() {
   int n, m;
   cin >> n >> m;
@@ -21,6 +34,8 @@ int main() {
       cin >> nums[i][j];
     }
   }
-  cout << "Ans: " << f(n-1, m-1, nums);
+  // cout << "Ans: " << f(n-1, m-1, nums);
+  vector<vector<int>> dp(n, vector<int>(m, -1));
+  cout << "Ans: " << f1(n-1, m-1, nums, dp);
   return 0;
 }
